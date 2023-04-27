8c4b8fbd0dd2b312a546420490bfc8cfef8983eb
Alex Montes <amontes261@icloud.com>
2023-04-26 20:32:09 -0400

    Revert "Moved ComingSoon code to Index Vuetify File"
    This reverts commit 865da041fb6229577d4ee26997d9e9398e010746.

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,83 @@
 <template>
-    <div class="text-center">
-        <div class="allContainer">
-            <v-card color="black" class="comingSoonCard">
-                <div class="textContainer">
-                    <div class="comingSoonHeader">
-                        Coming Soon
-                    </div>
-                    <div class= "smallTextContainer">
-                        <div> This webpage is still under development. </div>
-                        <div class= "completionAndCreatedByText divSpacer"> Expected completion: Fall 2023 </div>
-                        <div>
-                            In the meantime, you can check out the&nbsp;
-                            <a class= "repoLink" href="https://github.com/amontes261/mySite">repository</a>
-                            &nbsp;for updates.
-                        </div>
-                        <div> Thanks for coming & see you soon!</div>
-                        <div class= "completionAndCreatedByText divSpacer"> Created by Alex Montes </div>
-                        </div>
-                </div>
-            </v-card>
-        </div>
-    </div>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+        <NuxtLogo />
+        <VuetifyLogo />
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the Vuetify + Nuxt.js template
+        </v-card-title>
+        <v-card-text>
+          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+          <p>
+            For more information on Vuetify, check out the <a
+              href="https://vuetifyjs.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              documentation
+            </a>.
+          </p>
+          <p>
+            If you have questions, please join the official <a
+              href="https://chat.vuetifyjs.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="chat"
+            >
+              discord
+            </a>.
+          </p>
+          <p>
+            Find a bug? Report it on the github <a
+              href="https://github.com/vuetifyjs/vuetify/issues"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="contribute"
+            >
+              issue board
+            </a>.
+          </p>
+          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nuxt Documentation
+          </a>
+          <br>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nuxt GitHub
+          </a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
-<style>
-    .allContainer {
-        flex-direction: column;
-        font-size: calc(10px + 1.75vmin);
-        background-attachment: fixed;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .comingSoonCard {
-        color: black;
-    }
-
-    .completionAndCreatedByText{
-        font-size: calc(2px + 2vmin);
-    }
-
-    .comingSoonHeader {
-        font-weight: 800;
-        font-size: calc(60px + 5vmin);
-    }
-
-    .divSpacer{
-        height: 5vh;
-    }
-
-    .repoLink{
-        color:aqua;
-        text-decoration: none;
-    }
-
-    .smallTextContainer{
-        padding-top: 1em;
-    }
-
-    .textContainer{
-        color: white;
-        background-color: black;
-        padding-left: 1.5em;
-        padding-right: 1.5em;
-        padding-top: 1em;
-        padding-bottom: 1em;
-    }
-</style>
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
