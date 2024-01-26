abc25b8a747908d98234f2fc7356eebe4cc17165
Alex Montes <amontes261@icloud.com>
2024-01-26 10:22:43 -0500

    Add Resume Redirect
    - Added Vuetify Resume page that redirects to /resume.pdf
    - Changed resume to show correct timestamp in-title

---
+++ pages/resume.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="d-flex align-center justify-center pageContainer">
+        <img v-if="$vuetify.theme.dark" width="50" src="/logo_darkMode.png" />
+    </div>
+</template>
+
+<script>
+    
+
+    export default {
+        data () {
+            window.location.replace("https://alexmontes.net/resume.pdf"); 
+        },
+        head() {
+            return {
+                title: "Resume | AM"
+            };
+        }
+    }
+</script>
+
+<style>
+    .pageContainer{
+        height: 100vh;
+    }
+</style>
